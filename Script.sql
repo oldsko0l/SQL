e25c08417f6179dcee73883fab5741245faff8f9
oldsko0l <83029370+oldsko0l@users.noreply.github.com>
2026-02-11 19:16:52 -0300

    Add files via upload

---
+++ Script.sql
@@ -0,0 +1,22 @@
+--create database if exists supermecado;
+
+--create table cliente (
+--id int not null,
+--nome varchar(50) not null,
+--cpf varchar(11) not null,
+--constraint pk_id_cliente primary key (id)
+--)
+ 
+--create table produto(
+--id int not null,
+--descricao varchar(100) not null,
+--cod_barras varchar(14),
+--valor decimal(16,2)
+--)
+
+--alter table produto add constraint pk_id_produto primary key (id)
+
+--alter table produto add column situacao boolean not null l
+
+--alter table produto alter column descricao type varchar(200)
+
